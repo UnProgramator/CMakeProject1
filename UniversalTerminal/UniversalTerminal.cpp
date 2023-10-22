98aec1353479a67abeac78199f1230c3a1472d14
Pescaru Alexandru <apescaru11@yahoo.com>
2023-10-22 23:51:56 +0300

    update some targets and some bugs from rename (also added some code which will most probably get deleted

--- UniversalTerminal/UniversalTerminal.cpp
+++ UniversalTerminal/UniversalTerminal.cpp
@@ -1,14 +1,48 @@
 ﻿// CMakeProject1.cpp : Defines the entry point for the application.
 //
 
-#include "CMakeProject1.h"
+#include "UniversalTerminal.h"
 #include <cstdio>
+#include <cassert>
 
 #ifdef linux
 #include <unistd.h>
 #elif defined _WIN32
 #include <windows.h>
 #endif
+
+void convert(char* src, char from, char to) {
+	assert(from != '\0');
+	while (*src != '\0') {
+		if (*src == from)
+			*src = to;
+		src++;
+	}
+}
+
+void reduce(char* src, int prev = 1) {
+	if (prev == 0) return;
+	int idx1=-1, idx2=-1;
+	for (int i = 0; src[i] != '\0'; i++) {
+		if (src[i] == '/') {
+			if (idx1 == -1)
+				idx1 = i;
+			idx2 = i;
+		}
+	}
+
+	if (idx1 != idx2)
+		if (idx2 > idx1 + 3) {
+			//coverts the substring from first / to the last / to ..
+			strcpy(src + idx1 + 3, src + idx2);
+			*(int16_t*)(void*)(src + idx1 + 1) = 0x2E2E; //sets ".."
+		}
+		else { //no place for more than 1 .
+			src[idx1 + 1] = '.';
+			if (idx2 > idx1 + 2) 
+				src[idx1 + 2] = '.';
+		}
+}
 
 void getCurrentFolder(char* cwd, int buffsize) {
 #ifdef linux
@@ -22,6 +56,7 @@ void getCurrentFolder(char* cwd, int buffsize) {
 		printf("Error occured");
 		exit(-1);
 	}
+	convert(cwd, '\\', '/');
 #endif
 }
 
@@ -37,6 +72,8 @@ int main()
 
 	while (1) {
 		getCurrentFolder(cwd, sizeof(cwd));
+		printf("%s > \n", cwd);
+		reduce(cwd);
 		printf("%s > ", cwd);
 		
 		scanf("%s", comand);
